b463ead5a13d7ccc1103a3a9fedb91fd600e3d10
Dochi-svg <andrisuseno219@gmail.com>
2026-09-04 08:25:35 +0700

    Remove copyright and initialize debug service
    Removed copyright comments and added JLMemoryDebugService initialization.

--- app/src/main/java/ru/playsoftware/j2meloader/MainActivity.java
+++ app/src/main/java/ru/playsoftware/j2meloader/MainActivity.java
@@ -1,20 +1,3 @@
-/*
- * Copyright 2015-2016 Nickolay Savchenko
- * Copyright 2017-2018 Nikita Shakarun
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ru.playsoftware.j2meloader;
 
 import android.Manifest;
@@ -101,8 +84,12 @@ public class MainActivity extends BaseActivity {
 		if (dir.isDirectory() && dir.canWrite()) {
 			FileUtils.initWorkDir(dir);
 
-			// Inisialisasi TranslationManager
 			TranslationManager.init(dir);
+
+			JLMemoryDebugService debugService = new JLMemoryDebugService(8080);
+			Thread debugThread = new Thread(debugService);
+			debugThread.setDaemon(true);
+			debugThread.start();
 
 			appListModel.getAppRepository().onWorkDirReady();
 			return;
